aae353d0a0daea9e635fcc409293d5d30b8ff635
Duc Nguyen <ducnguyen2553@gmail.com>
2024-09-20 10:45:31 +0700

    rating

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -43,9 +43,20 @@ const getAllProducts = asyncHandler(async (req, res) => {
     }
 
     //Fields limiting
+    if (req.query.fields) {
+        const fields = req.query.fields.split(',').join(' ')
+        queryCommand = queryCommand.select(fields)
+    }
     //pagination
+    //limit :số  object lấy về 1 gọi api
+    //+2 => 2
+    //+adadsa => NaN
+    const page = +req.query.page || 1
+    const limit = +req.query.limit || process.env.LIMIT_PRODUCTS
+    const skip = (page - 1) * limit
+    queryCommand.skip(skip).limit(limit)
 
-    
+
     //Execute query
     //số lượng thỏa mãn điều kiện !== số lượng sp trả về 1 lần gọi API
     queryCommand.exec(async (err, response) => {
@@ -53,8 +64,8 @@ const getAllProducts = asyncHandler(async (req, res) => {
         const counts = await Product.find(formatedQueries).countDocuments()
         return res.status(200).json({
             success: response ? true : false,
-            products: response ? response : 'Cannot get  products',
-            counts
+            counts,
+            products: response ? response : 'Cannot get  products'
         })
     })
 
@@ -76,10 +87,39 @@ const deleteProduct = asyncHandler(async (req, res) => {
         updateProduct: deleteProduct ? deleteProduct : 'Cannot delete product'
     })
 })
+
+const ratings = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    const { star, comment, pid } = req.body
+    if (!star || !pid) throw new Error('Missing input')
+    const rantingProduct = await Product.findById(pid)
+    const alreadyRating = rantingProduct?.ratings?.find(element => element.postedBy.toString() === _id)
+    console.log(alreadyRating);
+    if (alreadyRating) {
+        //update star & comment
+        await Product.updateOne({
+            ratings: { $elMatch: alreadyRating }
+        },{
+            
+        })
+    } else {
+        //add star & comment
+        const response = await Product.findByIdAndUpdate(pid, {
+            $push: { ratings: { star, comment, postedBy: _id } }
+        }, { new: true })
+        console.log(response);
+    }
+    return res.status(200).json({
+        status: true,
+
+    })
+})
+
 module.exports = {
     createProduct,
     getProduct,
     getAllProducts,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    ratings
 }
